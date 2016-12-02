48236011ed370e1e7ef500b94edb0cea4ea810a7
AlvaroMoCa <almaseb_123@hotmail.com>
2016-12-02 14:32:33 -0500

    Nueva vercion sin login

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+//---------------------------
+var exame = require('./routes/exame');
+var practica = require('./routes/practica');
+var silabu = require('./routes/silabu');
+var trabajo = require('./routes/trabajo');
+//---------------------------
 
 var app = express();
 
@@ -24,6 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/exame', exame);
+app.use('/practica', practica);
+app.use('/silabu', silabu);
+app.use('/trabajo', trabajo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
